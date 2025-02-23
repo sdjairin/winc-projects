9998339bb6303ba6b7555a3a438bafc6c585aeae
sdjairin <168090493+sdjairin@users.noreply.github.com>
2025-02-23 13:45:04 +0100

    Update RecipeListPage.jsx

--- react-basics-project-adjusted/src/pages/RecipeListPage.jsx
+++ react-basics-project-adjusted/src/pages/RecipeListPage.jsx
@@ -36,73 +36,81 @@ export const RecipeListPage = ({ clickFn }) => {
     <>
       <SearchField searchField={searchField} handleChange={handleChange} />
       <Center h="auto" w="100vw" flexDir="row" flexWrap="wrap" gap={8}>
-        {matchedRecipes.map((recipe) => (
-          <Card
-            key={recipe.recipe.label}
-            w="sm"
-            h="xl"
-            borderRadius="xl"
-            bg="gray.50"
-          >
-            <CardBody cursor="pointer" onClick={() => clickFn(recipe)}>
-              <Image
-                src={recipe.recipe.image}
-                h={64}
-                w="sm"
-                borderRadius="xl"
-              />
-              <Stack mt="6" spacing="3">
-                <Heading size="md">{recipe.recipe.label}</Heading>
-                <Flex gap="2">
-                  {recipe.recipe.mealType.map((mealType) => (
-                    <Text key={mealType}> {mealType}</Text>
-                  ))}
-                </Flex>
-                <Flex gap="2">
-                  {recipe.recipe.dishType.map((dishType) => (
-                    <Text key={dishType}> {dishType}</Text>
-                  ))}
-                </Flex>
-                <Flex gap="2" color="blue.500">
-                  {recipe.recipe.dietLabels.map((dietLabel) => (
-                    <Badge
-                      key={dietLabel}
-                      colorScheme="purple"
-                      variant="subtle"
-                    >
-                      {" "}
-                      {dietLabel}
-                    </Badge>
-                  ))}
-                </Flex>
-                <Flex gap="2">
-                  {recipe.recipe.healthLabels.includes("Vegan") && (
-                    <Badge colorScheme="green" variant="outline">
-                      Vegan{" "}
-                    </Badge>
-                  )}
-                  {recipe.recipe.healthLabels.includes("Vegetarian") && (
-                    <Badge colorScheme="green" variant="outline">
-                      Vegetarian{" "}
-                    </Badge>
-                  )}
-                </Flex>
-                {recipe.recipe.cautions.length > 0 && (
+        {matchedRecipes.length > 0 ? (
+          matchedRecipes.map((recipe) => (
+            <Card
+              key={recipe.recipe.label}
+              w="sm"
+              h="xl"
+              borderRadius="xl"
+              bg="gray.50"
+            >
+              <CardBody cursor="pointer" onClick={() => clickFn(recipe)}>
+                <Image
+                  src={recipe.recipe.image}
+                  h={64}
+                  w="sm"
+                  borderRadius="xl"
+                />
+                <Stack mt="6" spacing="3">
+                  <Heading size="md">{recipe.recipe.label}</Heading>
                   <Flex gap="2">
-                    <Text>Cautions: </Text>
-                    <Grid gap={2} templateColumns="repeat(2, 1fr)">
-                      {recipe.recipe.cautions.map((caution) => (
-                        <Badge key={caution} colorScheme="red" variant="subtle">
-                          {caution}
-                        </Badge>
-                      ))}
-                    </Grid>
+                    {recipe.recipe.mealType.map((mealType) => (
+                      <Text key={mealType}> {mealType}</Text>
+                    ))}
                   </Flex>
-                )}
-              </Stack>
-            </CardBody>
-          </Card>
-        ))}
+                  <Flex gap="2">
+                    {recipe.recipe.dishType.map((dishType) => (
+                      <Text key={dishType}> {dishType}</Text>
+                    ))}
+                  </Flex>
+                  <Flex gap="2" color="blue.500">
+                    {recipe.recipe.dietLabels.map((dietLabel) => (
+                      <Badge
+                        key={dietLabel}
+                        colorScheme="purple"
+                        variant="subtle"
+                      >
+                        {" "}
+                        {dietLabel}
+                      </Badge>
+                    ))}
+                  </Flex>
+                  <Flex gap="2">
+                    {recipe.recipe.healthLabels.includes("Vegan") && (
+                      <Badge colorScheme="green" variant="outline">
+                        Vegan{" "}
+                      </Badge>
+                    )}
+                    {recipe.recipe.healthLabels.includes("Vegetarian") && (
+                      <Badge colorScheme="green" variant="outline">
+                        Vegetarian{" "}
+                      </Badge>
+                    )}
+                  </Flex>
+                  {recipe.recipe.cautions.length > 0 && (
+                    <Flex gap="2">
+                      <Text>Cautions: </Text>
+                      <Grid gap={2} templateColumns="repeat(2, 1fr)">
+                        {recipe.recipe.cautions.map((caution) => (
+                          <Badge
+                            key={caution}
+                            colorScheme="red"
+                            variant="subtle"
+                          >
+                            {caution}
+                          </Badge>
+                        ))}
+                      </Grid>
+                    </Flex>
+                  )}
+                </Stack>
+              </CardBody>
+            </Card>
+          ))
+        ) : (
+          <Text fontSize="2xl">No recipes found</Text>
+        )}
       </Center>
       <Box h={8} />{" "}
       {/* Add this Box component to add white space at the bottom */}
