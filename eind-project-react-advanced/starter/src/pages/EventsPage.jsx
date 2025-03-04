d6c593256358322b0df1a0d115c9ab9c166bd4d7
sdjairin <168090493+sdjairin@users.noreply.github.com>
2025-03-04 14:27:54 +0100

    Update EventsPage.jsx

--- eind-project-react-advanced/starter/src/pages/EventsPage.jsx
+++ eind-project-react-advanced/starter/src/pages/EventsPage.jsx
@@ -4,24 +4,25 @@ import { useLoaderData } from "react-router-dom";
 import EventList from "../components/EventList";
 
 export const loader = async () => {
-  const eventsResponse = await fetch("http://localhost:3000/events");
-  const categoriesResponse = await fetch("http://localhost:3000/categories");
+  try {
+    const eventsResponse = await fetch("http://localhost:3000/events");
+    const categoriesResponse = await fetch("http://localhost:3000/categories");
 
-  if (!eventsResponse.ok || !categoriesResponse.ok) {
+    const events = await eventsResponse.json();
+    const categories = await categoriesResponse.json();
+
+    return { events, categories };
+  } catch (error) {
+    console.error("Failed to fetch events or categories", error);
     throw new Error("Failed to fetch events or categories");
   }
-
-  const events = await eventsResponse.json();
-  const categories = await categoriesResponse.json();
-
-  return { events, categories };
 };
 
 export const EventsPage = () => {
   const { events, categories } = useLoaderData();
 
   return (
-    <Box>
+    <Box bg="white" p={4} h="100vh">
       <EventList events={events} categories={categories} />
     </Box>
   );
