edbdadfd196d32736efd6d9bbbf5bdd49e7294d0
sdjairin <168090493+sdjairin@users.noreply.github.com>
2025-03-14 11:37:38 +0100

    Delete express-book-app  directory

--- express-book-app /errors/NotFoundError.js
+++
@@ -1,8 +0,0 @@
-class NotFoundError extends Error {
-  constructor(resourceType, id) {
-    super(`${resourceType} with id ${id} not found`);
-    this.name = "NotFoundError";
-  }
-}
-
-export default NotFoundError;
--- express-book-app /middleware/NotFoundErrorHandler.js
+++
@@ -1,8 +0,0 @@
-const NotFoundErrorHandler = (err, req, res, next) => {
-  if (err.name === "NotFoundError") {
-    return res.status(404).send(err.message);
-  }
-  next(err);
-};
-
-export default NotFoundErrorHandler;
